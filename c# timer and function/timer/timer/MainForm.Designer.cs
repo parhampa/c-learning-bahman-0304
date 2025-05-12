7b2132dd17287538eef52e1411b63fe18f6f1097
parham ahmadipeymah <46413141+parhampa@users.noreply.github.com>
2025-05-12 07:34:15 +0330

    Add files via upload

---
+++ c# timer and function/timer/timer/MainForm.Designer.cs
@@ -0,0 +1,117 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: ASUS
+ * Date: 5/5/2025
+ * Time: 7:40 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace timer
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.PictureBox pictureBox2;
+		private System.Windows.Forms.PictureBox pictureBox3;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.pictureBox2 = new System.Windows.Forms.PictureBox();
+			this.pictureBox3 = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// timer1
+			// 
+			this.timer1.Enabled = true;
+			this.timer1.Interval = 1000;
+			this.timer1.Tick += new System.EventHandler(this.Timer1Tick);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(45, 180);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 22);
+			this.textBox1.TabIndex = 0;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.BackColor = System.Drawing.Color.Red;
+			this.pictureBox1.Location = new System.Drawing.Point(45, 12);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+			this.pictureBox1.TabIndex = 1;
+			this.pictureBox1.TabStop = false;
+			// 
+			// pictureBox2
+			// 
+			this.pictureBox2.BackColor = System.Drawing.Color.Green;
+			this.pictureBox2.Location = new System.Drawing.Point(45, 124);
+			this.pictureBox2.Name = "pictureBox2";
+			this.pictureBox2.Size = new System.Drawing.Size(100, 50);
+			this.pictureBox2.TabIndex = 1;
+			this.pictureBox2.TabStop = false;
+			this.pictureBox2.Visible = false;
+			// 
+			// pictureBox3
+			// 
+			this.pictureBox3.BackColor = System.Drawing.Color.Yellow;
+			this.pictureBox3.Location = new System.Drawing.Point(45, 68);
+			this.pictureBox3.Name = "pictureBox3";
+			this.pictureBox3.Size = new System.Drawing.Size(100, 50);
+			this.pictureBox3.TabIndex = 1;
+			this.pictureBox3.TabStop = false;
+			this.pictureBox3.Visible = false;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(200, 300);
+			this.Controls.Add(this.pictureBox3);
+			this.Controls.Add(this.pictureBox2);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.textBox1);
+			this.Name = "MainForm";
+			this.Text = "timer";
+			this.Load += new System.EventHandler(this.MainFormLoad);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+	}
+}
---
+++ c# timer and function/timer/timer/MainForm.cs
@@ -0,0 +1,90 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: ASUS
+ * Date: 5/5/2025
+ * Time: 7:40 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace timer
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	
+	public class mymsg
+	{
+		public void show(string inp)
+		{
+			MessageBox.Show(inp);
+		}
+		
+		private int calc(int a, int b){
+			return a+b;
+		}
+		public void calshow(){
+			this.show(this.calc(12,10).ToString());
+		}
+	}
+	
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		void MainFormLoad(object sender, EventArgs e)
+		{
+			mymsg tst=new mymsg();
+			//tst.show("salam");
+			tst.calshow();
+		}
+		void Timer1Tick(object sender, EventArgs e)
+		{
+			if(textBox1.Text=="")
+			{
+				textBox1.Text="0";
+			}else{
+				int tst=int.Parse(textBox1.Text);
+				tst++;
+				textBox1.Text=tst.ToString();
+			}
+			
+			int vaz=int.Parse(textBox1.Text);
+			if(vaz<30 && vaz>0){
+				pictureBox1.Visible=true;
+				pictureBox2.Visible=false;
+				pictureBox3.Visible=false;
+			}
+			if(vaz>30 && vaz<40){
+				pictureBox1.Visible=false;
+				pictureBox2.Visible=true;
+				pictureBox3.Visible=false;
+			}
+			if(vaz>40 && vaz<60){
+				pictureBox1.Visible=false;
+				pictureBox2.Visible=false;
+				pictureBox3.Visible=true;
+			}
+			if(vaz>60){
+				vaz=0;
+				textBox1.Text=vaz.ToString();
+				pictureBox1.Visible=true;
+				pictureBox2.Visible=false;
+				pictureBox3.Visible=false;
+			}
+		}
+	}
+}
